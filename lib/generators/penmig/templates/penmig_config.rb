1a1afae61a4c785635bb6fd60338de124c8d6ce7
Anoob Bava <anoob.bava@gmail.com>
2017-10-03 22:26:25 +0530

    require rake >> added for migrate

--- lib/generators/penmig/templates/penmig_config.rb
+++ lib/generators/penmig/templates/penmig_config.rb
@@ -3,6 +3,8 @@
 #IMPORTANT: DO NOT CHANGE THE RAILS_ENV TO production or test 
 # please keep this file as it is 
 #  if you want to remove the file  then run rails d pen-mig 
+require 'rake'
+
 if ENV['RAILS_ENV'] =='development'
   Rails.application.load_tasks
   Rake::Task['db:migrate'].invoke
